490aa95722b17d3049ef3580d531c7d952bb453d
prestonling <pling@ualberta.ca>
2022-09-12 19:15:22 -0600

    Update answers and python script

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -3,3 +3,4 @@ import requests as r
 if __name__ == "__main__":
     # requests does redirects on its own
     print(r.get("http://www.google.com/"))
+    print(r.get("https://raw.githubusercontent.com/prestonling/cmput404-labs/main/lab1/lab1.py"))
